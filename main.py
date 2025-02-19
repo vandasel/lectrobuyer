18b38ff88d9670b7fdee6904518d40421f840dc6
vandasel <kubavan02@gmail.com>
2025-02-19 15:52:42 +0100

    + widget ctk gui

--- main.py
+++ main.py
@@ -1,64 +1,147 @@
-import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import re
-# driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
-# title = driver.title
-
-# driver.implicitly_wait(0.5)
-
-# text_box = driver.find_element(by=By.NAME, value="my-text")
-# submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-# text_box.send_keys("Selenium")
-# submit_button.click()
-
-# message = driver.find_element(by=By.ID, value="message")
-# text = message.text
-
-# driver.quit()
+import customtkinter as ctk
+import pystray
+import sqlite3
+import threading
+import time
+from dbhandler import DbHandler
+from main import ElectroImporter  
+from PIL import Image
+from plyer import notification  
+from pystray import MenuItem as item, Icon
 
 
-class ElectroImporter:
-    
-    with open('mapping.json', 'r') as file:
-        MAPPING = json.load(file)
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")  
+WIDTH, HEIGHT = 800, 400
 
-    REGEX = r'www\.(.*)\.pl'
+class Widget(ctk.CTk):
 
-    def __init__(self,url):
-        self.url = url
-        self.driver = webdriver.Chrome()
-        self.title = self.get_title(url,self.REGEX)
 
-    @staticmethod
-    def get_title(url,pattern):
-        match = re.search(pattern,url)
-        if match:
-            text = match.group(1)    
-        if text:
-            return text
-        return "deferror"
+    def __init__(self):
+        super().__init__()
+        self.title("Electrobuyer")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
+        self.grid_columnconfigure(0, weight=1)  
         
+        self.fields = [] 
 
-    def get_price(self):
-        self.driver.get(self.url)
-        self.driver.implicitly_wait(0.5)
-        mapping = self.MAPPING.get(self.title,{})
-        button = WebDriverWait(self.driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, mapping.get("button")))
+     
+        self.input_frame = ctk.CTkFrame(self)
+        self.input_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+     
+        self.add_button = ctk.CTkButton(self, text="+", command=self.add_text_field)
+        self.add_button.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
+        
+     
+        self.submit_button = ctk.CTkButton(self, text="Submit", command=self.start_submit_thread)
+        self.submit_button.grid(row=2, column=0, padx=20, pady=20, sticky="ew")
+
+        self.protocol("WM_DELETE_WINDOW", self.hide_window)
+        self.load_previous_data()
+
+    def load_previous_data(self):
+        db = DbHandler()
+        previous_entries = db.get_last_price()
+        db.close()
+        
+        for _, link, status, price in previous_entries:
+
+            text_color = "red" if status == "Above Threshold" else "green" if status == "Below Threshold" else "gray"
+            
+            self.add_text_field(link, status, price, text_color)
+
+
+    def add_text_field(self, link="", status="NA", price="0", text_color="gray"):
+        field_frame = ctk.CTkFrame(self.input_frame)
+        field_frame.grid(row=len(self.fields), column=0, padx=10, pady=5, sticky="ew")
+
+        text_var = ctk.StringVar(value=link)  
+        text_field = ctk.CTkEntry(field_frame, textvariable=text_var, width=400)
+        text_field.grid(row=0, column=0, padx=5, pady=5)
+
+        status_label = ctk.CTkLabel(field_frame, text=status, text_color=text_color, width=100)
+        status_label.grid(row=0, column=1, padx=5, pady=5)
+
+        delete_button = ctk.CTkButton(field_frame, text="Delete", 
+                                    command=lambda: self.delete_field(field_frame))
+        delete_button.grid(row=0, column=2, padx=5, pady=5)
+
+        price_var = ctk.StringVar(value=str(price))
+        price_field = ctk.CTkEntry(field_frame, textvariable=price_var, width=50)
+        price_field.grid(row=0, column=3, padx=5, pady=5)
+
+        self.fields.append((field_frame, text_var, status_label, price_field))
+
+    def delete_field(self, field_frame):
+        for i, (frame, link, _,price) in enumerate(self.fields):
+            if frame == field_frame:
+                frame.destroy() 
+                del self.fields[i]
+                DbHandler().delete(link=link.get())
+                break
+
+       
+        self.reorganize_fields()
+    
+    def reorganize_fields(self):
+        for i, (frame, _, _,_) in enumerate(self.fields):
+            frame.grid(row=i, column=0, padx=10, pady=5, sticky="ew")
+    
+    def start_submit_thread(self):
+        threading.Thread(target=self.submit, daemon=True).start()
+
+    def submit(self):
+        for i, (_, text_var, status_label,price_w) in enumerate(self.fields):
+            link = text_var.get()  
+            try:
+                start = time.time()
+                price = ElectroImporter(link).run()  
+                end = time.time()
+                print(end-start)
+                if price < float(price_w.get()):
+                    status_label.configure(text="Below Threshold", text_color="green")
+                    self.show_notification(f"ALERT ALERT ALERT", f"{link} = {price}!!!!")
+                else:
+                    status_label.configure(text="Above Threshold", text_color="red")
+                
+                DbHandler().input_price(link=link,status=status_label._text, price=price_w.get())
+            except Exception as e:
+                status_label.configure(text="Error", text_color="orange")
+                print(f"Error fetching price for {link}: {e}")
+    
+    def auto_check_prices(self):
+        while True:
+            time.sleep(1200) 
+            self.submit()
+
+    def show_notification(self, title, message):
+        notification.notify(
+            title=title,
+            message=message,
+            timeout=10
         )
-        button.click()
-        price = self.driver.find_element(by=By.XPATH,value=mapping.get("price"))
-        self.driver.implicitly_wait(0.5)
-        return price.text
-    
 
-    def run(self):
-        return self.get_price()
-    
+    def hide_window(self):
+        self.withdraw()  
+        self.create_tray_icon()
 
-        
+    def create_tray_icon(self):
+        def restore_app(icon):
+            self.deiconify() 
+            icon.stop()  
+
+        def exit_app(icon):
+            icon.stop() 
+            self.quit()  
+
+        icon_image = Image.open("icon.PNG")
+
+        menu = (item("Restore", restore_app), item("Exit", exit_app))
+        self.tray_icon = Icon("Electrobuyer", icon_image, "Electrobuyer", menu)
+
+        threading.Thread(target=self.tray_icon.run, daemon=True).start()
+    
+if __name__ == "__main__":
+    app = Widget()
+    app.mainloop()
